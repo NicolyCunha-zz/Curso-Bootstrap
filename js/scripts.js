9a34eb626eb1b39ee7bdda99b4202f41c2744b38
Nicoly Cunha <nicolyoliveiracunha.2001@gmail.com>
2020-04-24 17:17:03 -0300

    Adicionando scroll da página e finalizando o projeto

--- js/scripts.js
+++ js/scripts.js
@@ -106,8 +106,7 @@ $(document).ready(function () {
             eachBoxes('all', boxes);
         }
 
-    })
-    
+    })    
 
     function eachBoxes(type,boxes) {
         if(type == 'all'){
@@ -122,5 +121,39 @@ $(document).ready(function () {
             })
         }
     }
+
+    //Scroll para seções
+    let navBtn = $('.nav-item');
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#services-area');
+    let teamSection = $('#team-area');
+    let portfolioSection = $('#portfolio-area');
+    let contactSection = $('#contact-area');
+    let scrollTo = '';
+
+    $(navBtn).click(function(){
+        let btnId = $(this).attr('id');
+        console.log(btnId);
+
+        if(btnId == 'about-menu'){
+            scrollTo = aboutSection;
+        } else if(btnId =='services-menu'){
+            scrollTo = servicesSection;
+        } else if(btnId =='team-menu'){
+            scrollTo = teamSection;
+        } else if(btnId =='portfolio-menu'){
+            scrollTo = portfolioSection;
+        } else if(btnId =='contact-menu'){
+            scrollTo = contactSection;
+        } else{
+            scrollTo = bannerSection;
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop:$(scrollTo).offset().top -70
+        }, 1500);
+    });
+
 
 });
